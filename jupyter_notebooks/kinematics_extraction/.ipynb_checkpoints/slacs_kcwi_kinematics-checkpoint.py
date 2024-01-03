90b1d76fb24b26dd7470097cdf9158131023f828
Shawn <shawnknabel@gmail.com>
2024-01-03 15:22:24 -0800

    testing slacs_kcwi_kinematics.py

--- jupyter_notebooks/kinematics_extraction/.ipynb_checkpoints/slacs_kcwi_kinematics-checkpoint.py
+++ jupyter_notebooks/kinematics_extraction/.ipynb_checkpoints/slacs_kcwi_kinematics-checkpoint.py
@@ -2,7 +2,6 @@ from astropy.io import fits
 import numpy as np
 import matplotlib.pyplot as plt
 import pathlib # to create directory
-import dill as pickle
 
 from ppxf.ppxf import ppxf
 from pathlib import Path
@@ -12,7 +11,6 @@ from scipy import ndimage
 from time import perf_counter as clock
 from scipy import interpolate
 from astropy.visualization import simple_norm
-from astropy.modeling.models import Sersic2D
 import astropy.units as u
 from vorbin.voronoi_2d_binning import voronoi_2d_binning
 
@@ -22,17 +20,9 @@ sys.path.append("/home/shawnknabel/Documents/slacs_kinematics/my_python_packages
 #register_sauron_colormap()
 from templates_util import Xshooter
 from kcwi_util import register_sauron_colormap
-from kcwi_util import visualization
-from kcwi_util import get_datacube
-from kcwi_util import ppxf_kinematics_RXJ1131_getGlobal_lens_deredshift
+from kcwi_util import poisson_noise
 from kcwi_util import find_nearest
 from kcwi_util import SN_CaHK
-from kcwi_util import select_region
-from kcwi_util import voronoi_binning
-from kcwi_util import get_voronoi_binning_data
-from kcwi_util import get_velocity_dispersion_deredshift
-from kcwi_util import kinematics_map
-from kcwi_util import stellar_type
 register_sauron_colormap()
 
 import ppxf.ppxf_util as ppxf_util
@@ -40,7 +30,7 @@ from os import path
 ppxf_dir = path.dirname(path.realpath(ppxf_util.__file__))
 import ppxf.sps_util as sps_util
 
-
+c = 299792.458 # km/s
 
 ##############
 
@@ -53,14 +43,14 @@ class slacs_kcwi_kinematics:
                  kcwi_datacube_file,
                  central_spectrum_file,
                  background_spectrum_file,
-                 z_lens,
+                 zlens,
                  exp_time,
                  lens_center_x,
                  lens_center_y,
                  aperture,
                  wave_min,
                  wave_max,
-                 degree
+                 degree,
                  sps_name,
                  pixel_scale,
                  FWHM,
@@ -69,7 +59,7 @@ class slacs_kcwi_kinematics:
                  radius_in_pixels,
                  SN,
                  plot,
-                 quiet,)
+                 quiet):
         
         self.mos_dir=mos_dir
         self.kin_dir=kin_dir
@@ -77,7 +67,7 @@ class slacs_kcwi_kinematics:
         self.kcwi_datacube_file=kcwi_datacube_file
         self.central_spectrum_file=central_spectrum_file
         self.background_spectrum_file=background_spectrum_file
-        self.z_lens=z_lens
+        self.zlens=zlens
         self.exp_time=exp_time
         self.lens_center_x=lens_center_x
         self.lens_center_y=lens_center_y
@@ -95,7 +85,25 @@ class slacs_kcwi_kinematics:
         self.plot=plot
         self.quiet=plot    
         
-###########################################################        
+###########################################################
+
+    def run_slacs_kcwi_kinematics(self, plot_bin_fits=False):
+        # to run all the steps at ones
+        print(f'pPXF will now consume your soul and use it to measure the kinematics of {self.obj_name}.')
+        self.datacube_visualization()
+        self.log_rebin_central_spectrum()
+        self.log_rebin_background_spectrum()
+        self.get_templates()
+        self.set_up_mask()
+        self.ppxf_central_spectrum()
+        self.crop_datacube()
+        self.create_SN_map()
+        self.select_region()
+        self.voronoi_binning()
+        self.ppxf_bin_spectra(plot_bin_fits=plot_bin_fits)
+        self.make_kinematic_maps()
+        self.plot_kinematic_maps()
+        print("Job's finished!")
         
     def datacube_visualization(self):
         '''
@@ -103,13 +111,14 @@ class slacs_kcwi_kinematics:
         :return: no return. This function is just for visualization
         '''
         # visualize the entire mosaic data
-        data_hdu = fits.open(self.kcwi_datacube)
+        data_hdu = fits.open(self.kcwi_datacube_file)
         datacube=data_hdu[0].data
         data_hdu.close()
         norm = simple_norm(np.nansum(datacube, axis=0), 'sqrt')
         plt.imshow(np.nansum(datacube, axis=0), origin="lower", norm=norm)
         plt.title('KCWI data')
         plt.colorbar(label='flux')
+        plt.pause(1)
 
     def log_rebin_central_spectrum(self):
         
@@ -124,13 +133,17 @@ class slacs_kcwi_kinematics:
         self.central_spectrum, self.rest_wave_log, self.central_velscale = ppxf_util.log_rebin(self.rest_wave_range, gal_lin)
         self.rest_wave = np.exp(self.rest_wave_log)
         
+    def log_rebin_background_spectrum(self):
+        # for now, no bakcground spectrum, just return None
+        self.background_spectrum = None
+        
     def get_templates(self):
         
         # take wavelength range of templates to be slightly larger than that of the galaxy restframe
         self.wave_range_templates = self.rest_wave_range[0]/1.2, self.rest_wave_range[1]*1.2
         
         # bring in the tempaltes
-        basename = f"spectra_{sps_name}_9.0.npz"
+        basename = f"spectra_{self.sps_name}_9.0.npz"
         filename = path.join(ppxf_dir, 'sps_models', basename)
         sps = sps_util.sps_lib(filename, 
                                self.central_velscale/self.velscale_ratio, 
@@ -138,6 +151,7 @@ class slacs_kcwi_kinematics:
                                wave_range=self.wave_range_templates)
         templates= sps.templates
         self.templates = templates.reshape(templates.shape[0], -1)
+        self.templates_wave = sps.lam_temp
 
     def set_up_mask(self):
         
@@ -160,30 +174,30 @@ class slacs_kcwi_kinematics:
         # Here the actual fit starts. The best fit is plotted on the screen.
         # Gas emission lines are excluded from the pPXF fit using the GOODPIXELS keyword.
         #
-        c = 299792.458
         vel = c*np.log(1 + 0)   # eq.(8) of Cappellari (2017)
         start = [vel, 250.]  # (km/s), starting guess for [V, sigma]
         #bounds = [[-500, 500],[50, 450]]
         t = clock()
 
         # create a noise
-        noise_array = np.full_like(galaxy, noise) # Assume constant noise per
+        noise_array = np.full_like(self.central_spectrum, self.noise) # Assume constant noise per
         
         pp = ppxf(self.templates, 
                   self.central_spectrum, 
                   noise_array, 
-                  self.velscale, 
+                  self.central_velscale, 
                   start, 
                   plot=True,
                   moments=2, 
                   goodpixels=self.mask,
                   degree=self.degree,
                   velscale_ratio=self.velscale_ratio,
-                  sky=self.background_source, 
+                  sky=self.background_spectrum, 
                   lam=self.rest_wave,
-                  lam_temp=self.wave_range_templates,
+                  lam_temp=self.templates_wave,
                  )
-        plt.xlim(wave_min*1e-4, wave_max*1e-4)
+        plt.xlim(self.wave_min*1e-4, self.wave_max*1e-4)
+        plt.ylim(np.nanmin(self.central_spectrum[self.mask])/1.1, np.nanmax(self.central_spectrum[self.mask])*1.1)
         plt.pause(1)
 
         print("Formal errors:")
@@ -194,7 +208,7 @@ class slacs_kcwi_kinematics:
         
         self.central_spectrum_ppxf = pp
         self.nTemplates = pp.templates.shape[1]
-        self.global_template = pp.templates @ pp.weights[:nTemplates]
+        self.global_template = pp.templates @ pp.weights[:self.nTemplates]
         self.global_template_wave = pp.lam_temp
         
     def crop_datacube(self):
@@ -206,13 +220,13 @@ class slacs_kcwi_kinematics:
         :return: new fits file with smaller size
         '''
         r = self.radius_in_pixels
-        data_hdu = fits.open(self.kcwi_datacube)
+        data_hdu = fits.open(self.kcwi_datacube_file)
         datacube=data_hdu[0].data
         data_hdu.close()
-        self.cropped_datacube = datacube[:, lens_center_y - r-1:lens_center_y +
-                                                              r, lens_center_x
+        self.cropped_datacube = datacube[:, self.lens_center_y - r-1:self.lens_center_y +
+                                                              r, self.lens_center_x
                                                                    - r -1:
-                                                                 lens_center_x + r]
+                                                                 self.lens_center_x + r]
         norm = simple_norm(np.nansum(self.cropped_datacube, axis=0), 'sqrt')
         plt.imshow(np.nansum(self.cropped_datacube, axis=0), origin="lower", norm=norm)
         plt.title('KCWI data')
@@ -221,7 +235,7 @@ class slacs_kcwi_kinematics:
     def create_SN_map(self):
 
         #estimate the noise from the blank sky
-        noise_from_blank = self.cropped_datcube[3000:4000, 4-3:4+2,4-3:4+2]
+        noise_from_blank = self.cropped_datacube[3000:4000, 4-3:4+2,4-3:4+2]
         std = np.std(noise_from_blank)
         s = np.random.normal(0, std, self.cropped_datacube.flatten().shape[0])
         noise_cube = s.reshape(self.cropped_datacube.shape)
@@ -232,8 +246,8 @@ class slacs_kcwi_kinematics:
         # region of CaH&K to estimate the S/N ratio (i.e. 4800AA - 5100AA).
         lin_axis_sky = np.linspace(self.rest_wave_range[0], self.rest_wave_range[1], self.cropped_datacube.shape[0])
         # find indices for SN
-        ind_min_SN = find_nearest(lin_axis_sky*(1+self.z), 4800)
-        ind_max_SN = find_nearest(lin_axis_sky*(1+self.z), 5100)
+        ind_min_SN = find_nearest(lin_axis_sky*(1+self.zlens), 4800)
+        ind_max_SN = find_nearest(lin_axis_sky*(1+self.zlens), 5100)
 
         # first, I need to estimate the flux/AA
         flux_per_half_AA = np.nanmedian(self.cropped_datacube[ind_min_SN:ind_max_SN, :, :],
@@ -250,7 +264,7 @@ class slacs_kcwi_kinematics:
         plt.show()
 
         # then, I estimate the noise/AA.
-        sigma_per_half_pixel = np.std(self.noise_cube[ind_min_SN:ind_max_SN,:,:], axis=0)
+        sigma_per_half_pixel = np.std(noise_cube[ind_min_SN:ind_max_SN,:,:], axis=0)
         sigma = np.sqrt(2) * sigma_per_half_pixel
         # some weired pattern so we find average in the black region around 36, 36
         #sigma_mean = np.mean(sigma [36-6:36+5, 36-6:36+5])
@@ -272,4 +286,200 @@ class slacs_kcwi_kinematics:
         plt.colorbar()
         plt.show()
 
-    
+    def select_region(self):
+        
+        SN_y_center, SN_x_center = np.unravel_index(self.SN_per_AA.argmax(), self.SN_per_AA.shape)
+        max_radius = 50
+        target_SN = 1.
+
+        xx = np.arange(self.radius_in_pixels * 2 + 1)
+        yy = np.arange(self.radius_in_pixels * 2 + 1)
+        xx, yy = np.meshgrid(xx, yy)
+        
+        dist = np.sqrt((xx - SN_x_center) ** 2 + (yy - SN_y_center) ** 2)
+
+        SN_mask = (self.SN_per_AA > target_SN) & (dist < max_radius)
+
+        xx_1D = xx[SN_mask]
+        yy_1D = yy[SN_mask]
+        SN_1D = self.SN_per_AA[SN_mask]
+        self.voronoi_binning_input = np.vstack((xx_1D, yy_1D, SN_1D, np.ones(SN_1D.shape[0])))
+
+        plt.imshow(SN_mask, origin="lower", cmap='gray')
+        plt.imshow(self.SN_per_AA, origin="lower", alpha=0.9)  #
+        plt.title('region selected for voronoi binning (S/N > %s)' % target_SN)
+        plt.axis('off')
+        plt.colorbar()
+        plt.show()
+        
+    def voronoi_binning(self):
+        
+        x, y, signal, noise = self.voronoi_binning_input
+        binNum, xNode, yNode, xBar, yBar, sn, nPixels, scale = voronoi_2d_binning(
+                x, y, signal, noise, self.SN, plot=1, quiet=1)
+        
+        plt.tight_layout()
+        #plt.savefig(target_dir + obj_name + '_voronoi_binning.png')
+        #plt.savefig(target_dir + obj_name + '_voronoi_binning.pdf') 
+        plt.pause(1)
+        plt.clf()
+        
+        self.voronoi_binning_output = np.column_stack([x, y,binNum])
+        ## get voronoi_binning_data based on the map
+        self.voronoi_binning_data = np.zeros((int(np.max(self.voronoi_binning_output.T[2]))+1,self.cropped_datacube.shape[0])) #construct the binning
+        #  data
+        check = np.zeros(self.cropped_datacube[0, :, :].shape)
+        for i in range(self.voronoi_binning_output.shape[0]):
+            #print(i)
+            wx = int(self.voronoi_binning_output[i][0])
+            wy = int(self.voronoi_binning_output[i][1])
+            num = int(self.voronoi_binning_output[i][2])
+            self.voronoi_binning_data[num]=self.voronoi_binning_data[num]+self.cropped_datacube[:,wy,wx]
+            check[wy, wx] = num+1
+
+        self.nbins = self.voronoi_binning_data.shape[0]
+        print("Number of bins =", self.nbins)
+        plt.imshow(check, origin="lower", cmap='sauron')
+        plt.colorbar()
+        #for (j, i), label in np.ndenumerate(check):
+        #    plt.text(i, j, label, ha='center', va='center')
+        plt.show()
+        
+    def ppxf_bin_spectra(self, plot_bin_fits=False):
+        self.bin_kinematics = np.zeros(shape=(0,5))
+        for i in range(self.nbins):
+            bin_spectrum = self.voronoi_binning_data[i]
+
+            galaxy, logLam1, velscale = ppxf_util.log_rebin(self.rest_wave_range, bin_spectrum)
+            lam = np.exp(logLam1)
+            
+            galaxy = galaxy[lam>self.wave_min] 
+            lam = lam[lam>self.wave_min]
+            galaxy = galaxy[lam<self.wave_max]
+            lam = lam[lam<self.wave_max]
+            logLam1 = np.log(lam)
+
+            lam_range_global_temp = np.array([self.global_template_wave.min(), self.global_template_wave.max()])
+            # after de-redshift, the initial redshift is zero.
+            goodPixels = ppxf_util.determine_goodpixels(np.log(lam), lam_range_global_temp, 0)
+
+            ind_min = find_nearest(np.exp(logLam1), self.wave_min)
+            ind_max = find_nearest(np.exp(logLam1), self.wave_max)
+
+            mask=goodPixels[goodPixels<ind_max]
+            mask = mask[mask>ind_min]
+
+            boolen = ~((2956 < mask) & (mask < 2983))  # mask the Mg II
+            mask = mask[boolen]
+            boolen = ~((2983 < mask) & (mask < 3001))  # mask the Mg II
+            mask = mask[boolen]
+            # Here the actual fit starts. The best fit is plotted on the screen.
+            # Gas emission lines are excluded from the pPXF fit using the GOODPIXELS keyword.
+            #
+            vel = c*np.log(1 + 0)   # eq.(8) of Cappellari (2017)
+            start = [vel, 250.]  # (km/s), starting guess for [V, sigma]
+            bounds = [[-500, 500],[50, 450]]
+            t = clock()
+
+            noise = np.full_like(galaxy, 0.0047)
+
+            pp = ppxf(self.global_template, galaxy, noise, velscale, start, plot=plot_bin_fits, quiet=self.quiet,
+                        moments=2, goodpixels=mask,
+                        degree=5,
+                        velscale_ratio=self.velscale_ratio,
+                        lam=lam,
+                        lam_temp=self.global_template_wave,
+                        )
+            if plot_bin_fits==True:
+                plt.pause(1)
+                plt.clf()
+            self.bin_kinematics = np.vstack((self.bin_kinematics, np.hstack( (pp.sol[:2],
+                                                           (pp.error*np.sqrt(pp.chi2))[:2],
+                                                                      pp.chi2) 
+                                                                      )
+                                         )
+                                        )
+            
+    def make_kinematic_maps(self):
+        
+        VD_array    =np.zeros(self.voronoi_binning_output.shape[0])
+        dVD_array   =np.zeros(self.voronoi_binning_output.shape[0])
+        V_array     =np.zeros(self.voronoi_binning_output.shape[0])
+        dV_array    =np.zeros(self.voronoi_binning_output.shape[0])
+
+
+        for i in range(self.voronoi_binning_output.shape[0]):
+            num=int(self.voronoi_binning_output.T[2][i])
+            vd = self.bin_kinematics[num][1]
+            dvd = self.bin_kinematics[num][3]
+            v = self.bin_kinematics[num][0]
+            dv = self.bin_kinematics[num][2]
+
+            VD_array[i]=vd
+            dVD_array[i]=dvd
+            V_array[i]=v
+            dV_array[i]=dv
+
+        final=np.vstack((self.voronoi_binning_output.T, VD_array, dVD_array, V_array, dV_array))
+
+        dim = self.radius_in_pixels*2+1
+
+        self.VD_2d=np.zeros((dim, dim))
+        self.VD_2d[:]=np.nan
+        for i in range(final.shape[1]):
+            self.VD_2d[int(final[1][i])][int(final[0][i])]=final[3][i]
+
+        self.dVD_2d=np.zeros((dim, dim))
+        self.dVD_2d[:]=np.nan
+        for i in range(final.shape[1]):
+            self.dVD_2d[int(final[1][i])][int(final[0][i])]=final[4][i]
+
+
+        self.V_2d=np.zeros((dim, dim))
+        self.V_2d[:]=np.nan
+        for i in range(final.shape[1]):
+            self.V_2d[int(final[1][i])][int(final[0][i])]=final[5][i]
+
+        self.dV_2d=np.zeros((dim, dim))
+        self.dV_2d[:]=np.nan
+        for i in range(final.shape[1]):
+            self.dV_2d[int(final[1][i])][int(final[0][i])]=final[6][i]
+
+    def plot_kinematic_maps(self):
+        # plot each
+        plt.figure()
+        plt.imshow(self.VD_2d,origin='lower',cmap='sauron')
+        cbar1 = plt.colorbar()
+        cbar1.set_label(r'$\sigma$ [km/s]')
+        #plt.savefig(target_dir + obj_name + '_VD.png')
+        plt.pause(1)
+        plt.clf()
+
+        plt.figure()
+        plt.imshow(self.dVD_2d, origin='lower', cmap='sauron',vmin=0, vmax=40)
+        cbar2 = plt.colorbar()
+        cbar2.set_label(r'd$\sigma$ [km/s]')
+        #plt.savefig(target_dir + obj_name + '_dVD.png')
+        plt.pause(1)
+        plt.clf()
+
+        # mean is bulk velocity, maybe?
+        mean = np.nanmedian(self.V_2d)
+        #
+        plt.figure()
+        plt.imshow(self.V_2d-mean,origin='lower',cmap='sauron',vmin=-100, vmax=100)
+        cbar3 = plt.colorbar()
+        cbar3.set_label(r'Vel [km/s]')
+        plt.title("Velocity map")
+        #plt.savefig(target_dir + obj_name + '_V.png')
+        plt.pause(1)
+        plt.clf()
+
+        plt.figure()
+        plt.imshow(self.dV_2d,origin='lower',cmap='sauron')
+        cbar4 = plt.colorbar()
+        cbar4.set_label(r'dVel [km/s]')
+        plt.title("error on velocity")
+        plt.pause(1)
+        plt.clf()
+        
